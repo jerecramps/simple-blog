431a23d669e863503cc16619a6c2f12cb953843c
jerecramps <jerecramps@gmail.com>
2020-08-05 05:31:18 +0800

    Revert "Revert "simple-blog-reactjs""
    This reverts commit 5c0378e914e8fe2f311f3030792c82acc53c1597.

---
+++ src/reducers/index.js
@@ -0,0 +1,50 @@
+import { CREATE_BLOG, DELETE_BLOG, UPDATE_BLOG } from '../constants';
+import { bake_cookie, read_cookie } from 'sfcookies';
+
+const blog = (action) => {
+  return {
+    title: action.title,
+    content: action.content,
+    datecreated: action.datecreated,
+    id: Math.random()
+  }
+}
+
+const removeById = (state = [], id) => {
+    const blogs = state.filter(blog => blog.id !== id);
+    return blogs;
+}
+
+const updateBlog = (state = [], action) => {
+  const blogs = state.map(blog => {
+    if(blog.id === action.id) {
+      blog = action;
+    }
+    return blog;
+  })
+
+  return blogs;
+}
+
+const blogs = (state=[], action) => {
+  let blogs = null;
+  state = read_cookie('blogs');
+  switch(action.type) {
+    case CREATE_BLOG:
+      blogs = [...state, blog(action)];
+      bake_cookie('blogs', blogs);
+      return blogs;
+    case DELETE_BLOG:
+      blogs = removeById(state, action.id);
+      bake_cookie('blogs', blogs);
+      return blogs;
+    case UPDATE_BLOG:
+      blogs = updateBlog(state, action);
+      bake_cookie('blogs', blogs);
+      return blogs;
+    default:
+      return state;
+  }
+}
+
+export default blogs;
